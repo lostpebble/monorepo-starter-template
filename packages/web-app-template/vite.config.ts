5736cac454c67e98e4ee8abb47811008274fa66f
Paul Myburgh <lostpebble@protonmail.com>
2025-09-15 02:21:43 +0200

    chore: update template

--- packages/web-app-template/vite.config.ts
+++ packages/web-app-template/vite.config.ts
@@ -4,5 +4,16 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  server: {
+    warmup: {
+      clientFiles: [
+        "./app/**/!(*.server|*.test)*.tsx", // Include all .tsx files except server and test files (add more patterns if required)
+        // "./app/entry.client.tsx",
+      ],
+    },
+  },
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  define: {
+    global: "globalThis",
+  },
 });
